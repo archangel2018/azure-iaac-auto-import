b32590eb484a83e56cac232137ceb5727630e087
Arun M <arun_m91@outlook.com>
2022-08-22 13:07:53 +0000

    Update Service Catalog Terraform: tf-poc-002

---
+++ tf-poc-002/terraform/default/module-tf-poc-002/variables.tf
@@ -0,0 +1,12 @@
+variable "azurerm_resource_group__subscriptions_97d20ca8_3e22_470f_8d26_e3c1fd97ca6e_resourcegroups_terraform_location" {
+  default = "eastus"
+}
+
+variable "azurerm_resource_group__subscriptions_97d20ca8_3e22_470f_8d26_e3c1fd97ca6e_resourcegroups_terraform_name" {
+  default = "terraform"
+}
+
+variable "azurerm_resource_group__subscriptions_97d20ca8_3e22_470f_8d26_e3c1fd97ca6e_resourcegroups_terraform_tc_category" {
+  default = "base"
+}
+
